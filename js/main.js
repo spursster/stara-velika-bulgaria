a312bfdb641c7fff476c84ec7b0a5f4f54586f92
spursster <spursster@gmail.com>
2026-05-11 10:08:01 +0300

    fs logic

--- js/main.js
+++ js/main.js
@@ -2,4 +2,9 @@ import { loadData, initGame } from './core/game.js';
 window.addEventListener('DOMContentLoaded', async () => {
   await loadData();
   initGame();
+  const fs = document.getElementById('fs');
+  if(fs) fs.onclick = () => {
+    if(!document.fullscreenElement) document.documentElement.requestFullscreen();
+    else document.exitFullscreen();
+  };
 });
