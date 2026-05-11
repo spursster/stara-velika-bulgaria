55fdf9c1838afe537ea1ab1727056b86127cbe72
spursster <spursster@gmail.com>
2026-05-11 11:31:04 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import { loadData, initGame } from './core/game.js';
+import { renderDynasties } from './systems/ritual.js';
 window.addEventListener('DOMContentLoaded', async () => {
   await loadData();
   initGame();
@@ -7,4 +8,5 @@ window.addEventListener('DOMContentLoaded', async () => {
     if(!document.fullscreenElement) document.documentElement.requestFullscreen();
     else document.exitFullscreen();
   };
+  renderDynasties();
 });
