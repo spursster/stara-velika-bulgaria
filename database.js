21ad87126b5683a34c0326fd022c01e64c198769
spursster <spursster@gmail.com>
2026-05-12 20:04:02 +0300

    Create database.js with dynasty information
    Added data for various historical dynasties including rulers and their stats.

---
+++ database.js
@@ -0,0 +1,39 @@
+const dynastyData = {
+    "Odrysians": {
+        founder: "Teres I",
+        startYear: -480,
+        rulers: [
+            { name: "Teres I", reign: [-480, -445], stats: { command: 8, diplomacy: 7 } },
+            { name: "Sitalkas", reign: [-431, -424], stats: { command: 10, diplomacy: 6 } },
+            { name: "Seuthes I", reign: [-424, -396], stats: { command: 7, diplomacy: 8 } },
+            { name: "Kotys I", reign: [-384, -360], stats: { command: 9, diplomacy: 7 } },
+            { name: "Seuthes III", reign: [-324, -312], stats: { command: 8, diplomacy: 9 } }
+        ]
+    },
+    "Macedons": {
+        founder: "Philip II",
+        rulers: [
+            { name: "Karan", reign: [-808, -778] },
+            { name: "Philip II", reign: [-359, -336], stats: { command: 10, diplomacy: 9 } },
+            { name: "Alexander III the Great", reign: [-336, -323], stats: { command: 11, diplomacy: 8 } }
+        ]
+    },
+    "Dulo": {
+        founder: "Avitohol",
+        rulers: [
+            { name: "Avitohol", reign: [153, 353], traits: ["Legendary", "Long-Lived"] },
+            { name: "Irnik", reign: [353, 453] },
+            { name: "Kurt Kubrat", reign: [632, 665] },
+            { name: "Asparuh", reign: [681, 701] },
+            { name: "Tervel", reign: [701, 721], title: "Caesar" },
+            { name: "Simeon the Great", reign: [893, 927], title: "Tsar" }
+        ]
+    },
+    "Bessarab": {
+        founder: "Basarab I",
+        rulers: [
+            { name: "Vlad III Tepes", reign: [1456, 1462], stats: { command: 10, fear: 10 } }
+        ]
+    }
+    // Тук ще добавим останалите 13 династии...
+};
