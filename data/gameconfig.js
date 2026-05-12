a94c8218ec63975a90fceb4d1a558f39ee2386b2
spursster <spursster@gmail.com>
2026-05-12 07:51:15 +0300

    Create gameconfig.js

---
+++ data/gameconfig.js
@@ -0,0 +1,10 @@
+// data/gameconfig.js
+window.GameConfig = {
+  defaultLanguage: 'bg',
+  startYear: 680,
+  map: {
+    width: 1024,
+    height: 768
+  },
+  // добави други настройки, които main.js очаква
+};
