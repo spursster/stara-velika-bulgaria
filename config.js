04f8b57e1c19152523bb948d3215f50013aa810a
spursster <spursster@gmail.com>
2026-05-12 17:25:53 +0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-window.GameConfig = {
-  startYear: -480, // 480 пр.н.е.
-  defaultLanguage: 'bg',
-  supportedLanguages: ['bg', 'en', 'ru'],
-  autosaveIntervalTurns: 5,
-};
-<script src="main.js?v=12"></script>
