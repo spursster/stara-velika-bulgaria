78122a8e7daf76cb92e736dcd58be566aa3641e6
spursster <spursster@gmail.com>
2026-05-15 15:27:29 +0300

    Refactor initialization and comments in logic.js

--- logic.js
+++ logic.js
@@ -1,21 +1,20 @@
 /**
  * МОДУЛ: ГЛАВНА ЛОГИКА - Велика България
- * СТАТУС: ФИКСИРАН (Старт: 480 пр.н.е.)
+ * СТАТУС: КОРИГИРАН (Старт: 480 пр.н.е.)
  */
 
 window.initNewGame = function() {
-    // 1. Инициализация на Кан
+    // 1. Данни за Кана
     window.currentHero = {
         name: "Кубрат", 
         dynasty: "Дуло",
         gold: 1500,
         armySize: 500,
         heroPower: 150,
-        age: 60,
-        techLevel: 1
+        age: 60
     };
 
-    // 2. ВРЕМЕ - Стриктно от 480 пр.н.е.
+    // 2. Време и Сезони (пр.н.е. логика)
     window.gameTime = { 
         year: -480, 
         seasonIndex: 0, 
@@ -26,7 +25,7 @@ window.initNewGame = function() {
     
     window.playerRegions = ["Крим"];
     
-    // Инициализация на 13-те рода
+    // Инициализация на рода в световните данни
     window.activeDynasties = {};
     if (window.bulgarianDynasties) {
         Object.keys(window.bulgarianDynasties).forEach(name => {
@@ -34,7 +33,7 @@ window.initNewGame = function() {
         });
     }
 
-    console.log("Логика: Играта стартира от 480 пр.н.е.");
+    console.log("Логика: Зареждане на играта от 480 пр.н.е.");
     if (window.updateCharacterUI) window.updateCharacterUI(window.currentHero);
 };
 
@@ -48,7 +47,7 @@ window.processTime = function() {
         if (window.currentHero) window.currentHero.age++;
     }
 
-    // Определяне на Ерата
+    // Определяне на Ерата спрямо годината
     let yr = window.gameTime.year;
     if (yr > 2100) window.gameTime.era = "КОСМИЧЕСКА ЕРА";
     else if (yr > 1900) window.gameTime.era = "ИНДУСТРИАЛНА ЕРА";
@@ -62,12 +61,14 @@ window.advanceTurn = function() {
     window.processTime();
     window.gameTime.turn++;
 
-    // Икономика
+    // Икономически бонус
     let seasonalBonus = (window.gameTime.seasonIndex === 2) ? 200 : 100;
     window.currentHero.gold += (window.playerRegions.length * seasonalBonus);
 
     if (window.updateCharacterUI) window.updateCharacterUI(window.currentHero);
 };
 
-// Изпълнение при зареждане
-window.onload = () => window.initNewGame();
+// Стартиране
+window.onload = () => {
+    window.initNewGame();
+};
