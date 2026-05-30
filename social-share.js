e36368a30636b0d10a0d973486c41d0e6e8211a8
spursster <spursster@gmail.com>
2026-05-31 00:58:36 +0300

    Create social-share.js

---
+++ social-share.js
@@ -0,0 +1,13 @@
+window.SocialShare = {
+  generateTikTokVideo: async function(hero) {
+    // целият код за видео
+  },
+  
+  shareToTwitter: function(hero) {
+    // ...
+  },
+  
+  shareToDiscord: function(hero) {
+    // ...
+  }
+};
